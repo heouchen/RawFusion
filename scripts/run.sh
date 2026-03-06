51d20d099d11fe2300876ea48bdf2e5517d11620
ChenWu <wc915495936@live.com>
2026-03-06 19:19:04 +0800

    03061919

--- scripts/run.sh
+++ scripts/run.sh
@@ -90,7 +90,10 @@ run_one () {
 # run_one "safnet_claude_33" "model_submit_claude33"
 # run_one "safnet_claude_34" "model_submit_claude34"
 # run_one "safnet_claude_35" "model_submit_claude35"
-run_one "safnet_claude_36" "model_submit_claude36"
-run_one "safnet_claude_37" "model_submit_claude37"
+# run_one "safnet_claude_36" "model_submit_claude36"
+# run_one "safnet_claude_37" "model_submit_claude37"
+run_one "safnet_claude_38" "model_submit_claude38"
+run_one "safnet_claude_39" "model_submit_claude39"
+run_one "safnet_claude_40" "model_submit_claude40"
 
 
